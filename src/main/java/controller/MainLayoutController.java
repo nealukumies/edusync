5fbe8a737274f369dee227ca5d5d3cd38c4307ae
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-09-19 16:07:19 +0300

    Created header logic

--- src/main/java/controller/MainLayoutController.java
+++ src/main/java/controller/MainLayoutController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
 public class MainLayoutController {
+    @FXML
+    private HBox headerContent;
+    @FXML
+    private VBox content;
+
+    public void initialize() {
+        changePage();
+    }
+
+    public void generateHeader() {
+        // Once we have a user model, this method will determine whether to show personal header or generic
+        // But for now, just use the generic "log in" header
+        headerContent.getChildren().clear();
+
+        // Dynamically forces all content to the right edge of the container
+        Region spacer = new Region();
+        headerContent.setHgrow(spacer, Priority.ALWAYS);
+
+        Label loginText = new Label("Log In");
+        loginText.getStyleClass().add("medium-title");
+        headerContent.getChildren().addAll(spacer, loginText);
+    }
+
+    // Will handle logic for swapping out content in the "content" VBox
+    public void changePage() {
+        generateHeader();
+    }
 }
