5489cb1bb3a276c76c7665973f0a2fa65ab04e7d
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-09-19 17:03:24 +0300

    Added Roboto Serif font

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -3,11 +3,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        // Load fonts
+        Font roboto = Font.loadFont(getClass().getResource("/font/RobotoSerif_28pt-Regular.ttf").toExternalForm(), 10);
+        System.out.println(roboto.getFamily());
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/MainLayout.fxml"));
         Parent root = fxmlLoader.load();
 
