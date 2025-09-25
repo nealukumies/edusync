087d86bb5edbf10d988b14e9af8e858c977d52e2
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-09-25 09:54:57 +0300

    Somewhat fixed inconsistent window size
    Now correctly displays window even if screen scaling is set to 125% (but not any higher than that).

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -1,28 +1,49 @@
 package view;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class MainView extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        // Load fonts
-        Font roboto = Font.loadFont(getClass().getResource("/font/RobotoSerif_28pt-Regular.ttf").toExternalForm(), 10);
-        //System.out.println(roboto.getFamily());
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/MainLayout.fxml"));
-        Parent root = fxmlLoader.load();
+  @Override
+  public void start(Stage stage) throws Exception {
+    // Load fonts
+    Font roboto = Font.loadFont(
+      getClass()
+        .getResource("/font/RobotoSerif_28pt-Regular.ttf")
+        .toExternalForm(),
+      10
+    );
+    //System.out.println(roboto.getFamily());
 
-        stage.setTitle("EduSync");
-        stage.setResizable(false);
-        stage.setScene(new Scene(root));
-        stage.show();
+    FXMLLoader fxmlLoader = new FXMLLoader(
+      getClass().getResource("/MainLayout.fxml")
+    );
+    Parent root = fxmlLoader.load();
+
+    if (Screen.getPrimary().getBounds() != null) {
+      Rectangle2D bounds = Screen.getPrimary().getBounds();
+      System.out.println(bounds.getWidth());
+      System.out.println(bounds.getHeight());
+      if (bounds.getWidth() < 1610 || bounds.getHeight() < 910) {
+        stage.setFullScreen(true);
+      }
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    stage.setTitle("EduSync");
+    stage.setResizable(false);
+    stage.setScene(new Scene(root, 1536, 864));
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
