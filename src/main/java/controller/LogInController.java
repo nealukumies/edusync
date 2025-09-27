474f6025ed29277b0d73def6d831192f629d15ac
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-09-27 16:42:37 +0300

    Added login functionality to UI

--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -2,10 +2,8 @@ package controller;
 
 import enums.Page;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import model.handlers.UserHandler;
 
 public class LogInController extends SubController {
     @FXML
@@ -16,11 +14,34 @@ public class LogInController extends SubController {
     private Button loginButton;
     @FXML
     private Hyperlink registerLink;
+    @FXML
+    private Label errorMessage;
 
     @Override
     public void initializeFully() {
         registerLink.setOnAction(e -> {
             this.getMainController().changePage(Page.REGISTER_PAGE);
         });
+        loginButton.setOnAction(e -> {
+            login();
+        });
+    }
+
+    public void login() {
+        String _email = email.getText();
+        String _password = password.getText();
+        try {
+            int id = UserHandler.loginUser(_email, _password);
+            if (id > -1) {
+                getMainController().changePage(Page.FRONT_PAGE);
+            }
+            else {
+                errorMessage.setText("Invalid email or password");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            errorMessage.setText(e.getMessage());
+        }
     }
 }
